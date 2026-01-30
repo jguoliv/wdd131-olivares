7ebc48bf00e6d38d89dc9e4e664051c6e14da260
jguoliv <julloqueolivares@gmail.com>
2026-01-29 19:27:27 -0700

    modal

--- week4/ponder/modal.js
+++ week4/ponder/modal.js
@@ -3,22 +3,26 @@ const modal = document.querySelector('dialog');
 const modalImage = modal.querySelector('img');
 const closeButton = modal.querySelector('.close-viewer');
 
-// Event listener for opening the modal
 gallery.addEventListener('click', openModal);
 
 function openModal(e) {
-    
-// Code to show modal  - Use event parameter 'e'   
-    
+    if (e.target.tagName !== 'IMG') return;
+
+    const smallSrc = e.target.src;
+    const largeSrc = smallSrc.replace('-sm', '-full');
+
+    modalImage.src = largeSrc;
+    modalImage.alt = e.target.alt;
+
+    modal.showModal();
 }
-// Close modal on button click
+
 closeButton.addEventListener('click', () => {
     modal.close();
 });
 
-// Close modal if clicking outside the image
-modal.addEventListener('click', (event) => {
-    if (event.target === modal) {
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
         modal.close();
     }
 });
