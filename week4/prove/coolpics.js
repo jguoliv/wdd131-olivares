6750a046cc675b26dd4d37bc018cc5774f957c44
jguoliv <julloqueolivares@gmail.com>
2026-01-31 16:26:05 -0700

    js

---
+++ week4/prove/coolpics.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('.menu-btn');
+const nav = document.querySelector('nav');
+
+
+btn.addEventListener('click', function() {
+    btn.classList.toggle('change');
+    nav.classList.toggle('show');
+});
+
+window.addEventListener('resize', function (){
+    const width = window.innerWidth;
+    if (width >= 700) {
+        btn.classList.remove('change');
+        nav.classList.remove('show');
+    }
+});
+
