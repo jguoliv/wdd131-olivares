97867c71e2cdded442e74fda1fbf6dec197be094
jguoliv <julloqueolivares@gmail.com>
2026-01-31 18:06:59 -0700

    js

--- week4/prove/coolpics.js
+++ week4/prove/coolpics.js
@@ -15,3 +15,38 @@ window.addEventListener('resize', function (){
     }
 });
 
+const gallery = document.querySelector('#gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    if (e.target.tagName !== 'IMG') return;
+
+    const smallSrc = e.target.src;
+    const largeSrc = smallSrc.replace('.jpg', '-full.jpg');
+
+    modal.classList.remove('show');
+
+    modalImage.src = largeSrc;
+    modalImage.alt = e.target.alt;
+
+    modalImage.offsetHeight;
+
+    modal.showModal();
+
+    modal.classList.add('show');
+}
+
+
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.close();
+    }
+});
